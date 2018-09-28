6e56190409424de9c03f5eec922fe3cf45299314
Sheldon O'Reilly <sheldon.oreilly@gmail.com>
2018-09-27 23:23:54 -0230

    mongoose connection

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//returns the module.export object - so pick the prop off of it
+const dbKey = require('./config/keys').mongoURI;
+
+mongoose.connect(dbKey).
+    then(() => console.log("DB Connection successful")).
+    catch((err) => console.log(err));
 
 app.get('/', (req, res) =>
 {
