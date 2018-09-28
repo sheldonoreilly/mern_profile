008086d3bb825cad04e596655d06a8f8b8f47031
Sheldon O'Reilly <sheldon.oreilly@gmail.com>
2018-09-28 03:15:08 -0230

    mongoose user model

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//create the user schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String,
+    required: true
+  },
+  data: {
+    type: Date,
+    required: true,
+    default: Date.now
+  }
+});
+
+//export User Model returned from mongoose
+module.exports = User = mongoose.model('users', UserSchema);
