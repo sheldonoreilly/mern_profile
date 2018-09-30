540e9a5a00114196e81b0c774bbb92f8388e8a54
Sheldon O'Reilly <sheldon.oreilly@gmail.com>
2018-09-30 16:23:00 -0230

    register form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,152 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import LockIcon from '@material-ui/icons/LockOutlined';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import withStyles from '@material-ui/core/styles/withStyles';
+
+const styles = theme => ({
+  layout: {
+    width: 'auto',
+    display: 'block', // Fix IE11 issue.
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
+      width: 500,
+      marginLeft: 'auto',
+      marginRight: 'auto'
+    }
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 8,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme
+      .spacing.unit * 3}px`
+  },
+  avatar: {
+    margin: theme.spacing.unit,
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: '100%', // Fix IE11 issue.
+    marginTop: theme.spacing.unit
+  },
+  submit: {
+    marginTop: theme.spacing.unit * 3
+  }
+});
 
 class Register extends Component {
+  state = {
+    name: '',
+    email: '',
+    password: '',
+    password_repeat: ''
+  };
+
+  handleChange = name => ({ target: { value } }) => {
+    console.log('value :', value);
+    console.log('name :', name);
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = e => {
+    e.preventDefault();
+    console.log('Form Submit');
+    console.log('this.state :', this.state);
+  };
+
   render() {
-    return <div>Register</div>;
+    const { classes } = this.props;
+    //destructure
+    const { name, email, password, password_repeat } = this.state;
+
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <main className={classes.layout}>
+          <Paper className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockIcon />
+            </Avatar>
+            <Typography variant="headline">Sign Up!</Typography>
+            <form className={classes.form}>
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="name">Name</InputLabel>
+                <Input
+                  onChange={this.handleChange('name')}
+                  id="name"
+                  name="name"
+                  value={name}
+                  autoComplete="name"
+                  autoFocus
+                />
+              </FormControl>
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="email">Email Address</InputLabel>
+                <Input
+                  onChange={this.handleChange('email')}
+                  id="email"
+                  name="email"
+                  value={email}
+                  autoComplete="email"
+                />
+              </FormControl>
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="password">Password</InputLabel>
+                <Input
+                  onChange={this.handleChange('password')}
+                  name="password"
+                  type="password"
+                  id="password"
+                  value={password}
+                />
+              </FormControl>
+              <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor="password">Confirm Password</InputLabel>
+                <Input
+                  onChange={this.handleChange('password_repeat')}
+                  name="password_repeat"
+                  type="password"
+                  id="password_repeat"
+                  value={password_repeat}
+                />
+              </FormControl>
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="raised"
+                color="primary"
+                className={classes.submit}
+                onClick={this.handleFormSubmit}>
+                Sign Up
+              </Button>
+            </form>
+          </Paper>
+        </main>
+      </React.Fragment>
+    );
   }
 }
 
-export default Register;
+Register.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Register);
