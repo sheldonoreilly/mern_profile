8270b4c9b4a1f9adb304b8f557bd024a3ea96b79
Sheldon O'Reilly <sheldon.oreilly@gmail.com>
2018-10-22 00:17:43 -0230

    production/deploy setting

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
+const path = require('path');
 
 const app = express();
 
@@ -31,4 +32,18 @@ app.use('/api/profile', profile);
 //passport
 app.use(passport.initialize());
 
-app.listen(5000, () => console.log('server listening on port 5000'));
+//serve static assests in production
+//in prod we dont have a server on the client (react dev server)
+//we instead use this one for client serves as well
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  //take everything other than listed '/api/...' routes
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server OK, listening on port ${port}`));
